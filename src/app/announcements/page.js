9bc52a8d462bb5e2c00ce51a726af4495430126d
Esteban Quintana <estebanquintana6@gmail.com>
2023-08-14 23:12:06 -0600

    annoucements component

--- src/app/announcements/page.js
+++ src/app/announcements/page.js
@@ -1,23 +1,17 @@
 import AnnouncementPreview from '@components/AnnouncementPreview'
 
 const fetchAnnouncements = async () => {
-  const response = await fetch('https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=30'); // TODO - change sample endpoint
-  const data = await response.json();
-  return data;
-}
-
-const getAnnouncements = async () => {
   try {
-    const result = await fetchAnnouncements();
-    return result;
+    const response = await fetch('https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=30'); // TODO - change sample endpoint
+    const data = await response.json();
+    return data;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 }
 
-
 const AnnouncementsPage = async() => {
-  const { blogs } = await getAnnouncements();
+  const { blogs } = await fetchAnnouncements();
   return (
     <>
       {blogs.map((announcement,  index) => <AnnouncementPreview key={index} announcement={announcement}></AnnouncementPreview>)}
