324a41208ea9243f99c93c31eb14af635a52c3fd
Esteban Quintana <estebanquintana6@gmail.com>
2023-08-19 20:08:14 -0600

    lint issues resolved

--- utils/ProtectedRoute.js
+++ utils/ProtectedRoute.js
@@ -6,12 +6,12 @@ import React, { useEffect } from "react";
 import { useAppSelector } from "@redux/hooks";
 
 export const ProtectedRoute = ({ children }) => {
-    const token = useAppSelector(({ authReducer }) => authReducer.token);
+    const token = useAppSelector(({ authReducer }) => authReducer.userToken);
     const router = useRouter();
 
     useEffect(() => {
         // checks if the user is authenticated
-        token === null && router.push("/");
+        console.log(token);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
